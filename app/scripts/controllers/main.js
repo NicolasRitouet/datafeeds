a854b04e07f69397e5b709eb21fd3b4c5767c4fa
NicolasRTT <nico2706@gmail.com>
2012-12-29 03:33:21 +0400

    Add addDatafeed Form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,22 @@
 'use strict';
 
 datafeedsApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+
+$scope.addDatafeed = function(datafeedTitle, datafeedUrl) {
+	$scope.datafeeds.push({title:datafeedTitle, url:datafeedUrl});
+};
+	var datafeed = {title: 'Zalando NL', url: 'http:/test.com'};
+	  $scope.datafeeds = [
+	{
+	title: 'Zalando',
+	url: 'http://www.zalando.de/feeds.xml'  
+	},
+	{title: 'La Redoute',
+	url: 'http://www.laredoute.fr'
+	},
+	{
+	title: 'Orange',
+	url: 'http://www.orange.fr'
+	}
+];
 });
