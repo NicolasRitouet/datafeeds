716497a2ddb0d82e0b38ce38d030861fb64dd5ad
NicolasRTT <nico2706@gmail.com>
2012-12-29 00:38:56 +0400

    Skeleton

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var datafeedsApp = angular.module('datafeedsApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+datafeedsApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
